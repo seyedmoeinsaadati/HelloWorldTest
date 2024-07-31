27cbd1ffc4a7238b4551e2077291de5044c5ad48
Seyed Moein Saadati <saadati.moin@gmail.com>
2024-08-01 01:51:36 +0330

    Load Cards in CardMatchingGameHandler.cs Add Card sprites into Factory.cs

--- Assets/Scripts/CardMatchingGame/CardMatchingGameHandler.cs
+++ Assets/Scripts/CardMatchingGame/CardMatchingGameHandler.cs
@@ -1,82 +1,98 @@
-using FlipFlop;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class CardMatchingGameHandler : MonoBehaviour
+namespace FlipFlop
 {
-    [Space]
-    [SerializeField] private GameStateManager gameManager;
-    [SerializeField] private Transform _cardContainer;
-    [SerializeField] private Card _cardPrefab;
-
-    ///////////////////////////////////////
-    /// STATIC MEMEBERS
-    ///////////////////////////////////////
-
-    public static Transform CardParent => Instance._cardContainer;
-
-    public static void StartGame()
+    public class CardMatchingGameHandler : MonoBehaviour
     {
-        GameInfo.Reset();
+        [Space]
+        [SerializeField] private GameStateManager gameManager;
+        [SerializeField] private Transform _cardContainer;
+        [SerializeField] private Card _cardPrefab;
 
-        Instance.gameManager.OpenGamePanel();
+        ///////////////////////////////////////
+        /// STATIC MEMEBERS
+        ///////////////////////////////////////
+        private static List<Card> Cards = new();
 
-        GameInfo.levelNumber = PlayerProfile.LevelIndex;
-
-        LoadLevel();
-        Instance.gameManager.OpenGamePanel();
-    }
-    public static void Win()
-    {
-        Clean();
-        PlayerProfile.LevelIndex++;
-        Instance.gameManager.OpenWinPanel();
-    }
-
-    public static void Lose()
-    {
-        Instance.gameManager.OpenLosePanel();
-    }
-
-    public static void BackToMainMenu()
-    {
-        Instance.gameManager.OpenMainMenu();
-    }
-
-
-    public static void LoadLevel()
-    {
-        var config = Factory.GetLevel(GameInfo.levelNumber);
-        GameInfo.time = config.timeLimit;
-
-        // spawn cards
-
-    }
-
-    /// </summary>
-    /// Clean the playground after finishing level
-    /// </summary>
-    public static void Clean()
-    {
-        // TODO: destroy all cards
-    }
-
-    // Singleton pattern
-    private static CardMatchingGameHandler instance;
-    public static CardMatchingGameHandler Instance
-    {
-        get
+        public static void StartGame()
         {
-            if (instance == null)
+            GameInfo.Reset();
+
+            Instance.gameManager.OpenGamePanel();
+
+            GameInfo.levelNumber = PlayerProfile.LevelIndex;
+
+            LoadLevel();
+            Instance.gameManager.OpenGamePanel();
+        }
+        public static void Win()
+        {
+            Clean();
+            PlayerProfile.LevelIndex++;
+            Instance.gameManager.OpenWinPanel();
+        }
+
+        public static void Lose()
+        {
+            Instance.gameManager.OpenLosePanel();
+        }
+
+        public static void BackToMainMenu()
+        {
+            Instance.gameManager.OpenMainMenu();
+        }
+
+
+        public static void LoadLevel()
+        {
+            var config = Factory.GetLevel(GameInfo.levelNumber);
+            GameInfo.time = config.timeLimit;
+
+            // spawn cards
+            Cards.Clear();
+            Sprite[] cardSprites = Factory.Sprites.GetSprites(config.numCard);
+            for (int i = 0; i < cardSprites.Length; i++)
             {
-                instance = FindObjectOfType<CardMatchingGameHandler>();
-                if (instance == null)
+                var card = Instantiate(Instance._cardPrefab, Instance._cardContainer);
+
+                if (card != null)
                 {
-                    instance = new GameObject().AddComponent<CardMatchingGameHandler>();
-                    instance.gameObject.name = instance.GetType().Name;
+                    card.Setup(i, cardSprites[i]);
+                    Cards.Add(card);
                 }
             }
+        }
 
-            return instance;
+        /// </summary>
+        /// Clean the playground after finishing level
+        /// </summary>
+        public static void Clean()
+        {
+            for (int i = Cards.Count - 1; i >= 0; i--)
+                Destroy(Cards[i]);
+
+            Cards.Clear();
+        }
+
+        // Singleton pattern
+        private static CardMatchingGameHandler instance;
+        public static CardMatchingGameHandler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<CardMatchingGameHandler>();
+                    if (instance == null)
+                    {
+                        instance = new GameObject().AddComponent<CardMatchingGameHandler>();
+                        instance.gameObject.name = instance.GetType().Name;
+                    }
+                }
+
+                return instance;
+            }
         }
     }
 }
--- Assets/Scripts/Factory.cs
+++ Assets/Scripts/Factory.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Factory : MonoBehaviour
 {
     [SerializeField] private List<LevelConfig> levels = new();
+    [SerializeField] private List<Sprite> cardSprites = new();
 
     private void Awake()
     {
@@ -14,6 +15,8 @@ public class Factory : MonoBehaviour
     ///////////////////////////////////////
     /// STATIC MEMEBERS
     ///////////////////////////////////////
+    private static Factory instance;
+
     public static LevelConfig GetLevel(int levelIndex)
     {
         if (instance != null)
@@ -24,6 +27,33 @@ public class Factory : MonoBehaviour
         return null;
     }
 
-    private static Factory instance;
+    public static class Sprites
+    {
+        public static Sprite[] GetSprites(int size)
+        {
+            int index = 0;
+            Sprite[] sprites = new Sprite[size];
+            for (int i = 0; i < size; i += 2)
+            {
+                sprites[i] = instance.cardSprites[index];
+                sprites[i + 1] = instance.cardSprites[index];
+                index++;
+            }
+            return Shuffle(sprites);
+        }
+
+        private static T[] Shuffle<T>(T[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                T temp = list[i];
+                int randomIndex = Random.Range(i, list.Length - 1);
+                list[i] = list[randomIndex];
+                list[randomIndex] = temp;
+            }
+            return list;
+        }
+    }
+
 
 }
