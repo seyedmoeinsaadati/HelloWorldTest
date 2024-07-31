1c84a6d3ee457b4e5441b56bdd04688bd76f1b98
Seyed Moein Saadati <saadati.moin@gmail.com>
2024-07-31 23:39:37 +0330

    Add Matching card game logic

---
+++ Assets/Scripts/GameLogic/GameLogic_MatchCard.cs
@@ -0,0 +1,14 @@
+﻿namespace FlipFlop
+{
+    public class GameLogic_MatchCard : GameLogicBase
+    {
+
+        public override void StartGame()
+        {
+        }
+
+        public override void FinishGame(int winner)
+        {
+        }
+    }
+}
