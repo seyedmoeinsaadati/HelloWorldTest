f7ef71646a428ea2fe2a4ddd2b30037baef98569
Seyed Moein Saadati <saadati.moin@gmail.com>
2024-07-31 21:08:08 +0330

    Add singltone GameManager to handle game state like main menu, playing, win, lose,

---
+++ Assets/Scripts/GameState/GameManager.cs
@@ -0,0 +1,44 @@
+using FlipFlop;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] private State_Playing playing;
+
+    public void Start()
+    {
+        SetupGamePanel();
+    }
+
+    public void SetupGamePanel()
+    {
+        playing.gameObject.SetActive(true);
+        playing.Init();
+
+    }
+
+    ///////////////////////////////////////
+    /// STATIC MEMEBERS
+    ///////////////////////////////////////
+
+    // Singleton pattern
+    private static GameStateManager instance;
+
+    public static GameStateManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameStateManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject().AddComponent<GameStateManager>();
+                    instance.gameObject.name = instance.GetType().Name;
+                }
+            }
+
+            return instance;
+        }
+    }
+}
