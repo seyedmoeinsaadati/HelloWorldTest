5ea748c3e6788e0efe9ae3bfa84ecdd38de03fbe
Seyed Moein Saadati <saadati.moin@gmail.com>
2024-08-01 16:09:20 +0330

    Add UiSoundButton to play ui click buttons

---
+++ Assets/Scripts/GameState/Playing/UiSoundButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FlipFlop
+{
+    public class UiSoundButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+
+        private void Start()
+        {
+            button.onClick.AddListener(PlayClickSound);
+        }
+
+        private void PlayClickSound()
+        {
+            AudioEventHandler.PlayUiClick();
+        }
+
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            button = GetComponent<Button>();
+        }
+#endif
+
+    }
+}
