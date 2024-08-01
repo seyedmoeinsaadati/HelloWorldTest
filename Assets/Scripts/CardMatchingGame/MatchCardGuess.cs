b1102dc598ff65e732722c074a5397c442d8d982
Seyed Moein Saadati <saadati.moin@gmail.com>
2024-08-01 11:43:22 +0330

    Add MatchCardGuess to store guessed card and state

---
+++ Assets/Scripts/CardMatchingGame/MatchCardGuess.cs
@@ -0,0 +1,37 @@
+﻿namespace FlipFlop
+{
+    [System.Serializable]
+    public class MatchCardGuess
+    {
+        private bool _active;
+        private Card _card;
+
+        public bool Active => _active;
+
+        public Card Card => _card;
+
+
+        public void Set(Card card)
+        {
+            _active = true;
+            _card = card;
+            _card.FlipUp();
+        }
+
+        public void Reset()
+        {
+            _card.FlipBack();
+            _active = false;
+            _card = null;
+        }
+
+        public void Clear()
+        {
+        }
+
+        public bool Equals(MatchCardGuess guess)
+        {
+            return _card.Id == guess._card.Id;
+        }
+    }
+}
