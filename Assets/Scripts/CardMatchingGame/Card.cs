985e5042e82edda8e0d15e4117c1d78a5faccba0
Seyed Moein Saadati <saadati.moin@gmail.com>
2024-08-01 11:15:05 +0330

    Add flip up and flip back methods into Card

--- Assets/Scripts/CardMatchingGame/Card.cs
+++ Assets/Scripts/CardMatchingGame/Card.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Utils;
 
 namespace FlipFlop
 {
@@ -8,15 +9,25 @@ namespace FlipFlop
         [SerializeField] private Image cardImage;
         [SerializeField] private Button button;
 
-        private int id = -1; // based on image (there are only 2 card with same id)
-        private int index = 0; // index of card in list
+        [SerializeField] private AnimationCurve flipCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
-        private Sprite sprite;
+        // based on image (there are only 2 card with same id)
+        private int _id = -1;
+
+        // index of card in list
+        private int _index = 0;
+
+        private Sprite _sprite;
+
+        private Coroutine _rotatingCoroutine;
+
+        public int Id => _id;
+        public int Index => _index;
 
         public void Setup(int index, int id, Sprite sprite)
         {
-            this.index = index;
-            this.id = id;
+            _index = index;
+            _id = id;
             name = "Card_" + id;
             cardImage.sprite = null;
 
@@ -32,12 +43,29 @@ namespace FlipFlop
 
         public void FlipUp()
         {
+            // TODO: Play sound
 
+            button.enabled = false;
+
+            if (_rotatingCoroutine != null) StopCoroutine(_rotatingCoroutine);
+            _rotatingCoroutine = this.DORotation(transform, new Vector3(0, 180, 0), .5f, .1f, flipCurve,
+            OnComplete: () =>
+            {
+                cardImage.sprite = _sprite;
+            });
         }
 
-        public void FlipDown()
+        public void FlipBack()
         {
+            // TODO: Play sound
 
+            if (_rotatingCoroutine != null) StopCoroutine(_rotatingCoroutine);
+            _rotatingCoroutine = this.DORotation(transform, new Vector3(0, 0, 0), .5f, .1f, flipCurve,
+            OnComplete: () =>
+            {
+                button.enabled = true;
+                button.image.sprite = null;
+            });
         }
 
 
